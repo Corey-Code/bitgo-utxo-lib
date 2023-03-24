e27fe6113c9bb45fc0ed92dd0213f87a14a4b407
Corey M <37006206+Corey-Code@users.noreply.github.com>
2023-03-24 09:05:32 -0500

    add cloreai

--- src/networks.js
+++ src/networks.js
@@ -637,6 +637,18 @@ module.exports = {
     coin: coins.BTC,
     hashFunctions: hashFunctions
   },
+  cloreai: {
+    messagePrefix: "\x18Clore Signed Message: \n",
+    bip32: {
+      public:  0x0488b21e,
+      private: 0x0488ade4,
+    },
+    pubKeyHash: 0x17,
+    scriptHash: 0x7a,
+    wif: 0x70,
+    coin: coins.BTC,
+    hashFunctions: hashFunctions
+  },
   bitcore: {
     messagePrefix: "\x18Bitcore Signed Message: \n",
     bech32: "btx", // not known
